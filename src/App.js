fc95ea6e3b04d9fe913e93e879897442396b651b
dinu <dinu.wijetunga@gmail.com>
2020-09-26 16:15:48 -0400

    pose tracker

--- src/App.js
+++ src/App.js
@@ -3,12 +3,62 @@ import "./App.css";
 import * as tf from "@tensorflow/tfjs";
 import * as posenet from "@tensorflow-models/posenet";
 import Webcam from "react-webcam";
+import { drawKeypoints, drawSkeleton } from "./utilities";
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  const runPosenet = async () => {
+    const net = await posenet.load({
+      inputResolution: { width: 640, height: 480 },
+      scale: 0.5,
+    });
+
+    setInterval(() => {
+      detect(net);
+    }, 100);
+  };
+
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      //get video properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      //set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      //Make detections
+      const pose = await net.estimateSinglePose(video);
+      console.log(pose);
+
+      drawCanvas(pose, video, videoWidth, videoHeight, canvasRef);
+    }
+  };
+
+  const drawCanvas = (pose, video, videoWidth, videoHeight, canvas) => {
+    const ctx = canvas.current.getContext("2d");
+    canvas.current.width = videoWidth;
+    canvas.current.height = videoHeight;
+
+    drawKeypoints(pose["keypoints"], 0.5, ctx);
+    drawSkeleton(pose["keypoints"], 0.5, ctx);
+  };
+
+  runPosenet();
+
   return (
     <div className="App">
       <header className="App-header">
         <Webcam
+          ref={webcamRef}
           style={{
             position: "absolute",
             marginLeft: "auto",
@@ -22,6 +72,7 @@ function App() {
         />
 
         <canvas
+          ref={canvasRef}
           style={{
             position: "absolute",
             marginLeft: "auto",
