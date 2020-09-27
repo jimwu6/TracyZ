80d0b08879824c7504d8c8393ad5452f4d2931a7
dinu <dinu.wijetunga@gmail.com>
2020-09-26 22:17:30 -0400

    Mirrored Camera

--- src/components/Points.js
+++ src/components/Points.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef } from "react";
 import Webcam from "react-webcam";
 import * as tf from "@tensorflow/tfjs";
 import * as posenet from "@tensorflow-models/handpose";
@@ -8,95 +8,96 @@ const handpose = require("@tensorflow-models/handpose");
 require("@tensorflow/tfjs-backend-webgl");
 
 const Points = () => {
-    const webcamRef = useRef(null);
-    const canvasRef = useRef(null);
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
 
-    const runPosenet = async () => {
-        const model = await handpose.load({
-        inputResolution: { width: 640, height: 480 },
-        scale: 0.25,
-        });
+  const runPosenet = async () => {
+    const model = await handpose.load({
+      inputResolution: { width: 640, height: 480 },
+      scale: 1,
+    });
 
-        setInterval(() => {
-        detect(model);
-        }, 100);
-    };
+    setInterval(() => {
+      detect(model);
+    }, 100);
+  };
 
-    const detect = async (model) => {
-        if (
-        typeof webcamRef.current !== "undefined" &&
-        webcamRef.current !== null &&
-        webcamRef.current.video.readyState === 4
-        ) {
-        //get video properties
-        const video = webcamRef.current.video;
-        const videoWidth = webcamRef.current.video.videoWidth;
-        const videoHeight = webcamRef.current.video.videoHeight;
+  const detect = async (model) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      //get video properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
 
-        //set video width
-        webcamRef.current.video.width = videoWidth;
-        webcamRef.current.video.height = videoHeight;
+      //set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+      //Make detections
+      const predictions = await model.estimateHands(
+        document.querySelector("video")
+      );
 
-        //Make detections
-        const predictions = await model.estimateHands(
-            document.querySelector("video")
-        );
-
-        if (predictions.length > 0) {
-            for (let i = 0; i < predictions.length; i++) {
-            const keypoints = predictions[i].landmarks;
-            // Log hand keypoints.
-            for (let i = 0; i < keypoints.length; i++) {
-                const [x, y, z] = keypoints[i];
-                //console.log(`Keypoint ${i}: [${x}, ${y}, ${z}]`);
-            }
-            drawCanvas(keypoints, video, videoWidth, videoHeight, canvasRef);
-            }
+      if (predictions.length > 0) {
+        for (let i = 0; i < predictions.length; i++) {
+          const keypoints = predictions[i].landmarks;
+          // Log hand keypoints.
+          for (let i = 0; i < keypoints.length; i++) {
+            const [x, y, z] = keypoints[i];
+            //console.log(`Keypoint ${i}: [${x}, ${y}, ${z}]`);
+          }
+          drawCanvas(keypoints, video, videoWidth, videoHeight, canvasRef);
         }
-        }
-    };
+      }
+    }
+  };
 
-    const drawCanvas = (keypoints, video, videoWidth, videoHeight, canvas) => {
-        const ctx = canvas.current.getContext("2d");
-        canvas.current.width = videoWidth;
-        canvas.current.height = videoHeight;
+  const drawCanvas = (keypoints, video, videoWidth, videoHeight, canvas) => {
+    const ctx = canvas.current.getContext("2d");
+    canvas.current.width = videoWidth;
+    canvas.current.height = videoHeight;
 
-        drawKeypoints(keypoints, 0.25, ctx);
-        //drawSkeleton(pose["keypoints"], 0.25, ctx);
-    };
-    
-    runPosenet();
+    drawKeypoints(keypoints, 1, ctx);
+    //drawSkeleton(pose["keypoints"], 0.25, ctx);
+  };
 
-    return (
-        <div>
-            <Webcam
-          ref={webcamRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            width: 640,
-            height: 480,
-          }}
-        />
-        <canvas
-          ref={canvasRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            width: 640,
-            height: 480,
-          }}
-        />
-        </div>    
-    );
+  runPosenet();
+
+  return (
+    <div>
+      <Webcam
+        ref={webcamRef}
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          width: 640,
+          height: 480,
+          transform: "scaleX(-1)",
+        }}
+      />
+      <canvas
+        ref={canvasRef}
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          width: 640,
+          height: 480,
+          transform: "scaleX(-1)",
+        }}
+      />
+    </div>
+  );
 };
 
 export default Points;
